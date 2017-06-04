b3f8d39ae34f57731899c9c6b2593398a9452cf6
ColinDorr <colin.maloud@gmail.com>
2017-06-04 20:23:03 +0200

    Made the system, to check for double inputs

--- TauwApp/routes/form.js
+++ TauwApp/routes/form.js
@@ -27,99 +27,30 @@ router.get('/', function(req, res, next) {
 
 router.post('/', function(req, res){
     data.checkForNewData(req, res);
-console.log("I have send the data")
-    // console.log("---------Formulier------------")
-    // console.log("---------Contact informatie------------")
-    // console.log(req.body.contactpersoon)
-    // console.log(req.body.email)
-    // console.log(req.body.mobile)
-    // console.log(req.body.profielfoto)
-    //
-    // console.log("---------Categorieën------------")
-    // console.log(req.body.sector)
-    //
-    // console.log("---------Vakgebied (waarin)------------")
-    // console.log(req.body.branch)
-    // console.log(req.body.tags)
-    // console.log(req.body.level)
-    //
-    // console.log("---------Details------------")
-    // console.log(req.body.scale)
-    // console.log(req.body.resolution)
-    // console.log(req.body.accuracy)
-    // console.log(req.body.interval)
-    // console.log(req.body.duration)
-    // console.log(req.body.innovation)
-    // console.log(req.body.costs)
-    //
-    // console.log("---------Beschrijving------------")
-    // console.log(req.body.samenvatting)
-    // console.log(req.body.referentieproject)
-    //
-    // console.log("--------------------------------")
-    // console.log("--------------------------------")
-
     res.render('form', { title: 'Formulier', filters: filters });
 });
 
-
 var data = {
     checkForNewData: function(req, res){
-        data.checkForNewSector(req, res);
-        data.checkForNewBranch(req, res);
-        data.checkForNewTags(req, res);
-    },
-    checkForNewSector: function (req, res) {
-        if (!(data.isInArray(req.body.sector[req.body.sector.length -1], filters.sector)) && req.body.sector.length > 1){
-            if (req.body.sector[req.body.sector.length -1] !== "undefined" && req.body.sector[req.body.sector.length -1] !==""){
-                filters.sector.push(req.body.sector[req.body.sector.length -1]);
-                console.log("-------------------SECTOR-----------------")
-                console.log(filters.sector)
-                console.log("------------------------------------------")
-            }
-        }
-    },
-    checkForNewBranch: function (req, res) {
-        if (!(data.isInArray(req.body.branch[req.body.branch.length -1], filters.branch)) && req.body.branch.length > 1){
-            if (req.body.branch[req.body.branch.length -1] !== "undefined" && req.body.branch[req.body.branch.length -1] !== ""){
-                filters.branch.push(req.body.branch[req.body.branch.length -1]);
-                console.log("-------------------BRANCH-----------------")
-                console.log(filters.branch)
-                console.log("------------------------------------------")
-            }
-        }
-    },
-    checkForNewTags: function (req, res) {
-        if (!(data.isInArray(req.body.tags[req.body.tags.length -1], filters.tags)) && req.body.tags.length > 1){
-            if (req.body.tags[req.body.tags.length -1] !== "undefined" && req.body.tags[req.body.tags.length -1] !== ""){
-                filters.tags.push(req.body.tags[req.body.tags.length -1]);
-                console.log("-------------------TAGS-----------------")
-                console.log(filters.tags)
-                console.log("------------------------------------------")
-            }
-        }
+        data.checkForNew(req, res, "sector"); // Check for new sectors, when form is posted.
+        data.checkForNew(req, res, "branch"); // Check for new branches, when form is posted.
+        data.checkForNew(req, res, "tags"); // Check for new tags, when form is posted.
     },
     isInArray: function(value, array) {
-        return array.indexOf(value) > -1;
+        return array.indexOf(value) > -1;   // Check the corresponding array, to see if there is already a item, that has the same value.
     },
+    checkForNew:function(req ,res, item){
+            var retrieved = req.body[item][req.body[item].length -1]
+            if (retrieved !== undefined && retrieved !== ''){   // Checks if the New input field is NOT undefined or empty.
+                if (!(data.isInArray((retrieved.toLowerCase().charAt(0).toUpperCase() + retrieved.toLowerCase().slice(1)), filters[item])) && req.body[item].length > 1){   // Check if the new value is already in the corresponding array, or not.
+                    filters[item].push(retrieved.toLowerCase().charAt(0).toUpperCase() + retrieved.slice(1)); // Take the userinput, change it all to lowercase and change only the first letter, to a uppercase.
+                    console.log("-------------------CHANGED-----------------") // Log the cahnges to the console/terminal.
+                    console.log("Added "+ retrieved.toLowerCase().charAt(0).toUpperCase() + retrieved.slice(1) + ", to "+ item + ".")
+                    console.log(req.body[item])
+                    console.log("------------------------------------------")
+                }
+            }
+        },
 };
-
-
-// checkForDoubleWords: function(listAll){ // Function, that checks if there are new words.
-//     if (index.params.length > wordList.length){ // the "index.params" values are from the userinput from the index.js script
-//         for (var i = 0; i < index.params.length; i++) {
-//             if (listAll.includes(index.params[i].toLowerCase()) == false){
-//                 wordList.push(index.params[i]);
-//             }
-//         }
-//     };
-//     data.generateWords(listAll);
-// },
-
-
-
-
-
-
 
 module.exports = {router: router, filters: filters};
