ed81b07ff62832b9487e27a179ad5be5ab242473
ColinDorr <colin.maloud@gmail.com>
2017-06-06 22:25:48 +0200

    The basic layout of the aside

--- TauwApp/routes/index.js
+++ TauwApp/routes/index.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', productList: productList});
 });
 
-module.exports = {router: router};
+var productList = {
+    branch : ["Bodem", "Afval", "Grondstoffen", "Ecologie", "Water", "Inspectie", "Waterbodem", "Infrastructuur"],
+    products: ["XRF", "NIR-handheld (Scio)", "Drone - RGB 3D model", "Mobiele GC", "Satelliet", "Medusa", "Grondradar"]
+};
+console.log(productList)
+
+module.exports = {router: router, productList: productList};
