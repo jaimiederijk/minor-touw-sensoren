b866e17f38fe48e0f4b7c8e0dc64619eecd105d5
jaimiederijk <j.r.derijk@quicknet.nl>
2017-06-09 00:49:34 +0200

    combine branches fixes

--- TauwApp/app.js
+++ TauwApp/app.js
@@ -48,7 +48,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 app.use('/form', form);
-app.use('/detail', detail);
+app.use('/details/:sensorName', detail);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
