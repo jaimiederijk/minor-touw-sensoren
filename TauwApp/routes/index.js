e1973e03d8f864fd1018cf703c836860ed22318d
ColinDorr <colin.maloud@gmail.com>
2017-06-08 14:58:03 +0200

    working version of the Aside

--- TauwApp/routes/index.js
+++ TauwApp/routes/index.js
@@ -1,5 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var fs = require("fs");
+
+// var JSONData = fs.readFileSync("./TauwSensoren.json");
+// JSONData = JSON.parse(JSONData)
+// console.log(JSONData)
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -10,6 +16,8 @@ var productList = {
     branch : ["Bodem", "Afval", "Grondstoffen", "Ecologie", "Water", "Inspectie", "Waterbodem", "Infrastructuur"],
     products: ["XRF", "NIR-handheld (Scio)", "Drone - RGB 3D model", "Mobiele GC", "Satelliet", "Medusa", "Grondradar"]
 };
-console.log(productList)
+
+
+// console.log(parsedJSON)
 
 module.exports = {router: router, productList: productList};
