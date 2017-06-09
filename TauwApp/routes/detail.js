a8d86d8df3d5ef61156b268207548e9f141afca9
jaimiederijk <j.r.derijk@quicknet.nl>
2017-06-09 11:46:35 +0200

    route name to detailpage

--- TauwApp/routes/detail.js
+++ TauwApp/routes/detail.js
@@ -2,8 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 
-router.get('/', function(req, res, next) {
-  res.render('detail', { title: 'XRF'});
+router.get('/:sensorName', function(req, res, next) {
+
+  res.render('detail', { title: req.params.sensorName});
 });
 
 module.exports = {router: router};
