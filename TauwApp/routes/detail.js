f459d38c820ff703d57478023da5573f5e45b6a3
ColinDorr <colin.maloud@gmail.com>
2017-06-12 17:22:10 +0200

    also working on detail

--- TauwApp/routes/detail.js
+++ TauwApp/routes/detail.js
@@ -1,22 +1,20 @@
 var express = require('express');
 var router = express.Router();
-
 var connector  = require('../lib/connector');
 
 router.get('/:sectorName/:sensorName', function(req, res, next) {
   var query = {
       sector: req.params.sectorName,
-      name: req.params.sensorName,
-      all: req.params
+      name: req.params.sensorName
   }
   connector.find( query, function(docs){
-      console.log(query)
     res.render('detail', {
-        sensor: query,
-        function(){console.log(sensor)}
+        title: 'homepage2',
+        sensor: docs
     });
-  } );
+  });
 });
+
 
 
 module.exports = {router: router};
--- TauwApp/routes/index.js
+++ TauwApp/routes/index.js
@@ -6,7 +6,6 @@ var connector  = require('../lib/connector');
 router.get('/', function(req, res, next) {
   res.render('index', {
       title: 'Home',
-      productList: productList,
       sensor: {sector: "nothing"}
   });
 });
@@ -19,18 +18,8 @@ router.get('/:sectorName', function(req, res, next) {
     console.log(docs)
     res.render('index', {
         title: 'homepage2',
-        productList: productList,
         sensor: docs
     });
   });
 });
-
-var productList = {
-    branch : ["Bodem", "Afval", "Grondstoffen", "Ecologie", "Water", "Inspectie", "Waterbodem", "Infrastructuur"],
-    products: ["XRF", "NIR-handheld (Scio)", "Drone - RGB 3D model", "Mobiele GC", "Satelliet", "Medusa", "Grondradar"]
-};
-
-
-// console.log(parsedJSON)
-
-module.exports = {router: router, productList: productList};
+module.exports = {router: router};
