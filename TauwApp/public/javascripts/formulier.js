6dde8db4c93be037d1bca509b3fd7b6920d12bd6
ColinDorr <colin.maloud@gmail.com>
2017-06-15 12:29:21 +0200

    Fixed #24, #31, #32 No more inline code or global variables

--- TauwApp/public/javascripts/formulier.js
+++ TauwApp/public/javascripts/formulier.js
@@ -1,4 +1,19 @@
-var loadFile = function(event) {
-  var output = document.getElementById('output');
-  output.src = URL.createObjectURL(event.target.files[0]);
-};
+(function(){
+
+    var profilePhoto = document.getElementById('profilePhoto')
+    var output = document.getElementById('output');
+
+    var loadFile = {
+        init: function () {
+            profilePhoto.addEventListener("keyup", function(){
+                loadFile.showPreview();
+            });
+        },
+        showPreview: function () {
+            output.src = profilePhoto.value;
+        },
+    };
+
+    loadFile.init();
+
+})();
