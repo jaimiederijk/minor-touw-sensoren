bc3dc6139afe3c5532d851694616086e1528b848
Nooroel Imamdi <nooroel.imamdi@hva.nl>
2017-07-04 00:16:41 +0200

    add imagePreview in CMS, not DRY yet

--- TauwApp/public/javascripts/imagePreview.js
+++ TauwApp/public/javascripts/imagePreview.js
@@ -1,17 +1,43 @@
 (function(){
 
-    var profilePhoto = document.getElementById('profilePhoto')
-    var output = document.getElementById('output');
+    // var inputProfilePhoto = document.querySelectorAll('input-profile-photo');
+    // var outputProfilePhoto = document.querySelectorAll('output-profile-photo');
+
+    var inputContactPf = document.querySelector('.input-contact-pf');
+    var outputContactPf = document.querySelector('.output-contact-pf');
+
+    var inputSensorImg = document.querySelector('.input-sensor-img');
+    var outputSensorImg = document.querySelector('.output-sensor-img');
+
+    var inputReferenceImage = document.querySelector('.input-reference-image');
+    var outputReferenceImage = document.querySelector('.output-reference-image');
+
+    // var inputContactPf = document.querySelector('.input-contact-pf');
+    // var outputContactPf = document.querySelector('.output-contact-pf');
+
+
 
     var loadFile = {
         init: function () {
-            profilePhoto.addEventListener("keyup", function(){
-                loadFile.showPreview();
-            });
+          inputSensorImg.addEventListener("keyup", function(){
+              // loadFile.showPreview();
+              outputSensorImg.src += inputSensorImg.value;
+
+          });
+          inputContactPf.addEventListener("keyup", function(){
+              // loadFile.showPreview();
+              outputContactPf.src += inputContactPf.value;
+
+          });
+          inputReferenceImage.addEventListener("keyup", function(){
+              // loadFile.showPreview();
+              outputReferenceImage.src += inputReferenceImage.value;
+
+          });
         },
-        showPreview: function () {
-            output.src = profilePhoto.value;
-        },
+        // showPreview: function () {
+        //     outputSensorImg.src = inputSensorImg.value;
+        // },
     };
 
     loadFile.init();
