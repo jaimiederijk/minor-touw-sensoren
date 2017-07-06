68af491707229201e124b479811959123d3039b8
Nooroel Imamdi <nooroel.imamdi@hva.nl>
2017-07-06 17:47:35 +0200

    when img upload is error

--- TauwApp/public/javascripts/errorImagePreview.js
+++ TauwApp/public/javascripts/errorImagePreview.js
@@ -3,12 +3,12 @@
     var imgError = {
         fallback: function() {
           var outputImg = document.querySelectorAll('.output-img');
-          var urlImageMistake = document.querySelector('.url-img-failed');
+          var urlImageMistake = document.querySelectorAll('.url-img-failed');
           for (var i = 0; i < outputImg.length; i++) {
             outputImg[i].onerror = function(e) {
               // e.target.parentElement.querySelector('.output-img').src = "/images/tauw_logo_zonder.png";
               e.target.parentElement.querySelector('.output-img').classList.add('hide');
-              urlImageMistake.classList.remove('hide');
+              e.target.parentElement.querySelector('.url-img-failed').classList.remove('hide');
             }
           }
           // for (var i = 0; i < outputImg.length; i++) {
